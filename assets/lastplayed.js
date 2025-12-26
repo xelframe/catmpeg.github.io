c8126250c8d5141bb7e23c37937542d7c65233dc
xelframe <bhzrd0@gmail.com>
2025-12-26 20:56:05 +0300

    ааа ето что

--- assets/lastplayed.js
+++ assets/lastplayed.js
@@ -1,13 +1,15 @@
-const lastPlayedBlock = document.getElementById('last-played');
+document.addEventListener("DOMContentLoaded", () => {
+    const lastPlayedBlock = document.getElementById('last-played');
 
-fetch("https://lastfm-last-played.biancarosa.com.br/catmpeg/latest-song")
-    .then(response => response.json())
-    .then(data => {
-        if (data != 0) {
-            const lastPlayed = document.querySelector("#song");
-            lastPlayed.innerHTML = `<a target="_blank" href="${data.track.url}">
-                ${data.track.name} - ${data.track.artist["#text"]}</a>`;
-            lastPlayedBlock.style.display = "block";
-        }
-    })
-    .catch(err => console.error(err));
+    fetch("https://lastfm-last-played.biancarosa.com.br/catmpeg/latest-song")
+        .then(response => response.json())
+        .then(data => {
+            if (data != 0) {
+                const lastPlayed = document.getElementById("song");
+                lastPlayed.innerHTML = `<a target="_blank" href="${data.track.url}">
+                    ${data.track.name} - ${data.track.artist["#text"]}</a>`;
+                lastPlayedBlock.style.display = "block";
+            }
+        })
+        .catch(err => console.error(err));
+});
