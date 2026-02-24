3eaa073127bb3be8babd77d7cb0d32d1290866d2
catmpeg <bhzrd0@gmail.com>
2026-02-24 13:51:37 +0300

    не долго catmpeg прожил

--- assets/lastplayed.js
+++ assets/lastplayed.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const lastPlayedBlock = document.getElementById('last-played');
 
-    fetch("https://cat.fs.tlpteam.ru:5011/?user=catmpeg")
+    fetch("https://cat.fs.tlpteam.ru:5011/?user=xelframe")
         .then(response => response.json())
         .then(data => {
             if (data && data.name) {
